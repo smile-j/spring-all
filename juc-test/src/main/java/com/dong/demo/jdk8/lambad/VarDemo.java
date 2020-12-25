c2437258fdaea68a0fd22408c28be95af71e357a
dongjunpeng <junpeng.dong@causacloud.com>
2020-12-25 08:11:12 +0800

    1

--- juc-test/src/main/java/com/dong/demo/jdk8/lambad/VarDemo.java
+++ juc-test/src/main/java/com/dong/demo/jdk8/lambad/VarDemo.java
@@ -1,9 +1,14 @@
 package com.dong.demo.jdk8.lambad;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 /**
  * 变量引用
+ *
+ * java 传的是值，不是引用
  */
 public class VarDemo {
 
@@ -11,11 +16,18 @@ public class VarDemo {
 
     public static void main(String[] args) {
 
-        String str= "hello";
-//        str = "";
-        Consumer<String> consumer = (s)-> System.out.println(s+str);
-        consumer.accept("123");
+//        String str= "hello";
+////        str = "";
+//        Consumer<String> consumer = (s)-> System.out.println(s+str);
+//        consumer.accept("123");
 
+        HashMap map = new HashMap();
+        map.put("","");
+
+        Hashtable hashtable = new Hashtable();
+        ConcurrentHashMap<String,String> stringStringConcurrentHashMap = new ConcurrentHashMap<>();
+        stringStringConcurrentHashMap.put("","");
+        stringStringConcurrentHashMap.size();
     }
 
 }
